8aaa90cbc52bbd8caf4af3678d150f273fbcd9c8
SebastianSciorra <sebastiansciorra@gmail.com>
2024-05-15 19:34:54 -0300

    agregar navbar draft

---
+++ src/components/navbar/menu.jsx
@@ -0,0 +1,13 @@
+import './menu.css'
+
+function Menu() {
+    return (
+        <nav>
+        <h1>MiEcommerce</h1>
+        <button>inicio</button>
+        <button>lapiz</button>
+        </nav>
+    )
+}
+
+export default Menu
